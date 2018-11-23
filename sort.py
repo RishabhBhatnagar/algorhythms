faee6449d9d14f17c8733a1f9c6e640f69abb75b
bhushan-borole <37565807+bhushan-borole@users.noreply.github.com>
2018-11-23 11:31:06 +0530

    radix sort added

--- sort.py
+++ sort.py
@@ -145,3 +145,50 @@ def heap_sort(array):
 # end heap sort
 
 
+#radix sort
+def __counting_sort(A, mod): 
+  
+    n = len(A) 
+  
+    # intitalize final output array
+    output = [0] * n
+  
+    # initialize count array as 0 
+    count = [0] * 10
+  
+    
+    for i in range(0, n): 
+        index = A[i] // mod 
+        count[index % 10] += 1
+  
+    for i in range(1,10): 
+        count[i] += count[i-1] 
+  
+    # Build the output array 
+    i = n-1
+    while i>=0: 
+        index = A[i] // mod 
+        output[ count[index % 10] - 1] = A[i] 
+        count[index % 10] -= 1
+        i -= 1
+  
+    #copying elements from output to original array
+    i = 0
+    for i in range(0,len(A)): 
+        A[i] = output[i] 
+
+    return A
+
+  
+
+def radix_sort(array): 
+    max_of_list = max(array) 
+
+    mod = 1
+    while max_of_list / mod > 0: 
+        array = __counting_sort(array, mod) 
+        mod *= 10
+    return array
+# end radix sort
+
+
